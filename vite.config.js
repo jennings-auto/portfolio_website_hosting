c2ad9bf2884a4176ac78b9af81c4542cd8277e3c
Sanimar-Singh <sanimar.manghera@spit.ac.in>
2024-10-28 19:47:45 +0530

    initial upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio_website_hosting/',
+  base: '/sanimar_portfolio/',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
